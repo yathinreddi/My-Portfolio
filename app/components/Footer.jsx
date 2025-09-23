73bf4584d0f45573a50e4ca9750e51c444f04978
yathinreddi <yathinbommineni@gmail.com>
2025-09-23 19:01:20 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -10,18 +10,45 @@ const Footer = ({isDarkMode}) => {
 
         <div className='w-max flex items-center gap-2 mx-auto'>
         <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6' />
-        yathinbommineni@gmail.com
+        <a
+            href="mailto:yathinbommineni@gmail.com"
+            className='hover:underline'
+          >
+            yathinbommineni@gmail.com
+          </a>
         </div>
       </div>
-      <div className='text-center sm:flex items-center justify-center border-t border-gray-400 mx-[2%] mt-10 py-6'>
-        
+      <div className='text-center sm:flex items-center justify-center border-t border-gray-400 mx-[2%] mt-10 py-6'>        
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="Yathin Reddy <yathinbommineni@gmail.com>">LinkedIn</a></li>
-            <li><a target='_blank' href="Yathin Reddy <yathinbommineni@gmail.com>">Gmail</a></li>
-            <li><a target='_blank' href="">Instagram</a></li>
-
+            <li>
+              <a 
+                href="https://www.linkedin.com/in/yathin-bommineni-n210/"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:underline'
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:yathinbommineni@gmail.com"
+                className='hover:underline'
+              >
+                Gmail
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/yathinreddi"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:underline'
+              >
+                GitHub
+              </a>
+            </li>
         </ul>
-
       </div>
     </div>
   )
