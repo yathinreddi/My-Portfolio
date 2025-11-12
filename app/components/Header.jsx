de5283490e48b7ebc402ac1e69b4c6a078a280b3
yathinreddi <yathinbommineni@gmail.com>
2025-11-12 19:53:10 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
         initial={{y: 30, opacity: 0}}
         whileInView={{y: 0, opacity: 1}}
         transition={{duration: 0.6, delay:0.7}}
-        href="/Resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>
+        href="/Resume- Yathin Reddy.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>
             My Resume<Image src={assets.download_icon} alt='' className='w-4'/>
         </motion.a>
         
@@ -60,3 +60,4 @@ const Header = () => {
 }
 
 export default Header
+
